ee7928e2840ad64e05f7021d69900531fb65f893
AyushGenii <143549107+AyushGenii@users.noreply.github.com>
2023-09-11 03:17:02 +0530

    Add files via upload

---
+++ leapyearcheck.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("write year= ");
+scanf("%d",&a);
+if(a%400==0){
+	printf("It is a leap year");
+}
+else if(a%100==0)
+{
+printf("It is not a leap year");
+}	
+else if("a%4==0")
+{
+	printf("It is not a leap year");
+}
+else{
+	printf("It is not a lea year");
+}
+}
