d26f6ca4c90b3aec43415265ef99187d2be969db
AyushGenii <143549107+AyushGenii@users.noreply.github.com>
2023-11-01 19:02:27 +0530

    Update electricitybill.cpp

--- electricitybill.cpp
+++ electricitybill.cpp
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a,b;
+int a,b,c,d;
 printf("NUMBER OF UNITS= ");
 scanf("%d",&a);
-b=a*10;
-printf("\nYour bill is %d",b);
+b=5;
+c=100;
+printf("Chargeper unit=Rs.%d ",b);
+printf("Surcharge=Rs.%d ",c);
+  
+
+d=a*b+c;
+printf("\nYour bill is %d",d);
 return 0;
 }
