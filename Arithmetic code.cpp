4910d150645996c5ae8ee1ca5f2deb91d1897bf0
AyushGenii <143549107+AyushGenii@users.noreply.github.com>
2023-11-01 17:43:21 +0530

    Update Arithmetic code.cpp

--- Arithmetic code.cpp
+++ Arithmetic code.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-int a,b,c,d,e;
+int a,b,c,d,e,f;
 printf("Write a\n ");
 scanf("%d",&a);
 
@@ -10,14 +10,18 @@ scanf("%d",&b);
 printf("The sum is  ");
 c=a+b;
 printf("%d",c);
+	
+printf("\nThe Difference is  ");
+d=a-b;
+printf("%d",d);
 
 printf("\nThe product is  ");
 d=a*b;
-printf("%d",d);
+printf("%d",e);
 
 printf("\nThe Dividend is  ");
 e=a/b;
-printf("%d",e);
+printf("%d",f);
 return 0;
 
 
