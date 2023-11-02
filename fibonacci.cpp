e7759addbb68c105a58061253894ae533fc888a4
AyushGenii <143549107+AyushGenii@users.noreply.github.com>
2023-11-03 00:00:30 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>;
+int main(){
+	int a,b,c;
+	c=0;
+	b=1;
+	int count=2;
+	scanf("%d",&a);
+	
+	while(count<a){
+		int temp=b;
+		b=b+c;
+		c=temp;
+		count++;
+	}
+	printf("%d",b);
+}
